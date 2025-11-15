18640e1f5e81d698cdc5cd32b7c8b49f7a315ff3
efensyjn <me@efensyjn.moe>
2025-11-15 19:46:46 +0500

    Loading bar

--- index.js
+++ index.js
@@ -2,72 +2,66 @@ const cardListEl = document.querySelector(".card-list");
 const buttonEl = document.querySelector(".button");
 const inputEl = document.querySelector(".input");
 const searchedEl = document.querySelector(".movie__searched--name");
-const spinnerEl = document.querySelector(".fa-spinner");
 let executed = false;
 let inputSearch = false;
 let mainCount = 0;
 
+const loader = document.getElementById("loader");
+
+function showLoader() {
+  loader.classList.add("show");
+}
+
+function hideLoader() {
+  loader.classList.remove("show");
+}
+
 function onSearchMovie(event) {
   const movieData = event.target.value;
   searchedEl.innerHTML = movieData;
-  inputSearch = true;
-  executed = true;
-  spinnerEl.classList += " display-block";
-  search(movieData);
+  search();
 }
 
-function search(movieData) {
-  if (executed) {
-    main(movieData);
+function search() {
+  const movieData = inputEl.value.trim();
+
+  if (!movieData) {
+    inputEl.classList.add("input-shake");
+    buttonEl.classList.add("input-shake");
+    setTimeout(() => {
+      inputEl.classList.remove("input-shake");
+      buttonEl.classList.remove("input-shake");
+    }, 600);
+    return;
   }
 
-  executed = false;
+  cardListEl.innerHTML = "";
+  showLoader();
 
-  // input properties
-
-  if (!inputSearch) {
-    inputEl.classList += " input-shake";
-    buttonEl.classList += " input-shake";
-  }
-  setTimeout(() => {
-    inputEl.classList.remove("input-shake");
-    buttonEl.classList.remove("input-shake");
-  }, 600);
-  setTimeout(() => {
-    inputSearch = false;
-  }, 700);
+  main(movieData);
 }
 
 async function userInterface() {
   if ((mainCount == 0)) {
-    const data = await fetch(
-      `https://www.omdbapi.com/?apikey=c968a92&s=marvel`
-    );
-    const jsonData = await data.json();
-    console.log(jsonData)
-    cardListEl.innerHTML = jsonData.Search.map((movie) => cardHTML(movie)).join(
-      ""
-    );
+    main("marvel")
   }
   mainCount++;
 }
 userInterface()
 
 async function main(movieData) {
+  showLoader();
   const data = await fetch(
     `https://www.omdbapi.com/?apikey=c968a92&s=${movieData}`
   );
   const jsonData = await data.json();
+  hideLoader();
   if ((jsonData.Response === "False")) {
     cardListEl.innerHTML = movieNotFound();
   }
-  console.log(jsonData);
-  spinnerEl.classList.remove("display-block");
-  if ((jsonData.Response === "True")) {
-    cardListEl.innerHTML = jsonData.Search.map((movie) => cardHTML(movie)).join(
-      ""
-    );
-  }
+  cardListEl.innerHTML = jsonData.Search.map((movie) => cardHTML(movie)).join(
+    ""
+  );
 }
 
 function movieNotFound() {
